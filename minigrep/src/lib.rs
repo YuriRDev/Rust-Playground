e0f96fa20802489f9fe4f146a545c57beb16ab60
Yuri <49888082+YuriRDev@users.noreply.github.com>
2023-05-01 14:11:03 -0300

    `MiniGrep` - Case sensitive search, default is false

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub struct Config {
-    query: String,
-    filename: String,
+    pub query: String,
+    pub filename: String,
+    pub sensitive: bool,
 }
 
 impl Config {
@@ -14,21 +15,43 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        return Ok(Config { query, filename });
+        return Ok(Config {
+            query,
+            filename,
+            sensitive: false,
+        });
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
-        println!("{line}");
+    if config.sensitive {
+        for line in search_sensitive(&config.query, &contents) {
+            println!("{line}");
+        }
+    } else {
+        for line in search(&config.query, &contents) {
+            println!("{line}");
+        }
     }
 
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+pub fn search_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut result = Vec::new();
 
     for line in contents.lines() {
