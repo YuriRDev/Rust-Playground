c9b16fa822db021e4263eafbdf1626e317c71f50
Yuri <49888082+YuriRDev@users.noreply.github.com>
2023-05-03 15:13:41 -0300

    `Lib.rs` - Book struct

--- lib_system/src/lib.rs
+++ lib_system/src/lib.rs
@@ -1,5 +1,49 @@
-struct Book {
-    id: i32, 
+use std::io;
+
+pub struct Book {
     name: String,
     description: String,
+}
+
+impl Book {
+    pub fn new(name: String, description: String) -> Book {
+        Book { name, description }
+    }
+
+    pub fn new_verbose() -> Book {
+        let mut name = String::new();
+        let mut description = String::new();
+
+        io::stdin()
+            .read_line(&mut name)
+            .expect("Could not read the name!");
+
+        io::stdin()
+            .read_line(&mut description)
+            .expect("Could not read the description!");
+
+        Book {
+            name: name.trim().to_string(),
+            description: description.trim().to_string(),
+        }
+    }
+}
+
+impl Book {
+    pub fn print(&self) {
+        println!("{}", self.name);
+        println!("{}", self.description)
+    }
+}
+
+pub enum Action {
+    DELETE(i32),
+    CREATE(Book),
+    LIST,
+    UPDATE(i32, Book),
+}
+
+pub fn get_action() {
+    let mut input = String::new();
+
 }
--- lib_system/src/main.rs
+++ lib_system/src/main.rs
@@ -1,4 +1,16 @@
+use lib_system::*;
 fn main() {
-    
+    let mut array: Vec<Book> = Vec::new();
 
+    let mut a = Book::new(String::from("Nome"), String::from("Description"));
+
+    get_action();
+
+    list(&array);
+}
+
+fn list(vector: &Vec<Book>) {
+    for book in vector {
+        book.print();
+    }
 }
